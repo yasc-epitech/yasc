9b3fce3301d07380631a0aa239b747aaff1c7e03
Hugo Barbier <zegfault42@gmail.com>
2014-12-27 22:30:02 +0100

    Change path for static files to /client folder for Heroku

--- server/config/express.js
+++ server/config/express.js
@@ -44,6 +44,7 @@ module.exports = function(app) {
   if ('production' === env) {
     //app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     //app.use(express.static(path.join(config.root, 'public')));
+    app.use(express.static(path.join(config.root, 'client')));
     app.set('appPath', config.root + '/client');
     app.use(morgan('dev'));
   }
