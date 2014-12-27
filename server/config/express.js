9c12b02ed128a2b2279b77f7dcd59a9a230ac230
Hugo Barbier <zegfault42@gmail.com>
2014-12-27 22:31:34 +0100

    Change path for app to client for Heroku

--- server/config/express.js
+++ server/config/express.js
@@ -45,7 +45,7 @@ module.exports = function(app) {
     //app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     //app.use(express.static(path.join(config.root, 'public')));
     app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', config.root + '/client');
+    app.set('appPath', '/client');
     app.use(morgan('dev'));
   }
 
