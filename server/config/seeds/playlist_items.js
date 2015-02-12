6fda9f05d1976cb760892c5d053399adaf75556d
Zegfault <zegfault42@gmail.com>
2015-02-12 15:57:33 +0100

    Fix seeds

--- server/config/seeds/playlist_items.js
+++ server/config/seeds/playlist_items.js
@@ -10,13 +10,27 @@ var Track = require('../../api/track/track.model');
 var PlaylistItem = require('../../api/playlist_item/playlist_item.model');
 
 
+
+var playlist_1 = Playlist.findOne({ title: 'Chill' }, function(error, playlist) {
+return playlist;
+});
+var playlist_2 = Playlist.findOne({ title: 'Melodic' }, function(error, playlist) {
+return playlist;
+});
+var track_1 = Track.findOne({ url: '68571849' }, function(error, track) {
+return track;
+});
+var track_2 = Track.findOne({ url: '58706008' }, function(error, track) {
+return track;
+});
+
 PlaylistItem.find({}).remove(function() {
   PlaylistItem.create({
-    playlist:  Playlist.findOne({ title: 'Chill' })._id,
-    track:  Track.findOne({ url: '68571849' })._id
+    playlist:  playlist_1._id,
+    track:  track_1._id
   }, {
-    playlist:  Playlist.findOne({ title: 'Melodic' })._id,
-    track:  Track.findOne({ url: '58706008' })._id
+    playlist:  playlist_2._id,
+    track:  track_2._id
   }, function() {
     console.log('Finished populating playlist_items');
   });
