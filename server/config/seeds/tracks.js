e4ea18322145848cad6ab710b15d5c22faeac84f
Mathieu Marques <mathieumarques78@gmail.com>
2015-02-12 02:23:35 +0100

    Add track detail page

--- server/config/seeds/tracks.js
+++ server/config/seeds/tracks.js
@@ -10,19 +10,19 @@ var User = require('../../api/user/user.model');
 
 User.findOne({ email: 'admin@admin.com' }, function(error, user) {
   Track.create({
-    title:       'Musique de fou',
-    description: 'description track 1',
-    url:         'http://soundcloud.com/forss/flickermood',
-    artwork_url: 'http://posey.com/artwork_url',
-    duration:    240,
-    author:      'Anakin'
+    title:       'Booba - Wesh Morray',
+    description: 'Bah ouais Morray',
+    url:         '68571849',
+    artwork_url: '',
+    duration:    -1,
+    author:      'heaton-karafi'
   }, {
-    title:       'Dou-double Poney Swag',
-    description: 'description track 2',
-    url:         'http://soundcloud.com/forss/flickermood',
-    artwork_url: 'http://posey.com/artwork_url',
-    duration:    180,
-    author:      'Dark Vador'
+    title:       'Savant - Starfish',
+    description: '',
+    url:         '58706008',
+    artwork_url: '',
+    duration:    -1,
+    author:      'aleksander-vinter'
   });
 });
 console.log('Finished populating tracks');
